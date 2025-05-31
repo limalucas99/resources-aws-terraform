62f423c997e727980d0484189ceb61eab80f897c
Gabriel Angelo <73532594+angelog@users.noreply.github.com>
2025-05-31 13:12:55 -0300

    adding suffix to bucket name

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,5 +1,9 @@
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "this" {
-  bucket = var.bucket_name
+  bucket = "${var.bucket_name}-${random_id.suffix.hex}"
 
   tags = var.tags
 }
