53cabab4a1f77ecdd3ec4da07c39f162679eff8c
Gabriel Angelo <73532594+angelog@users.noreply.github.com>
2025-05-31 13:17:36 -0300

    add resource stage

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -25,10 +25,13 @@ resource "aws_api_gateway_integration" "lambda" {
   uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${var.lambda_invoke_arn}/invocations"
 }
 
-
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [aws_api_gateway_integration.lambda]
   rest_api_id = aws_api_gateway_rest_api.this.id
-  stage_name  = "dev"
 }
 
+resource "aws_api_gateway_stage" "stage" {
+  stage_name    = "dev"
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
+}
