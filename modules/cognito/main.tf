f472f1721d945f776f6aa203dcdbcdb1d7df4783
Gabriel Angelo <73532594+angelog@users.noreply.github.com>
2025-06-03 11:54:52 -0300

    explicit auths

--- modules/cognito/main.tf
+++ modules/cognito/main.tf
@@ -3,7 +3,14 @@ resource "aws_cognito_user_pool" "this" {
 }
 
 resource "aws_cognito_user_pool_client" "client" {
-  name         = "${var.user_pool_name}-client"
-  user_pool_id = aws_cognito_user_pool.this.id
-  generate_secret = false
+  name             = "${var.user_pool_name}-client"
+  user_pool_id     = aws_cognito_user_pool.this.id
+  generate_secret  = false
+
+  explicit_auth_flows = [
+    "ADMIN_NO_SRP_AUTH",
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH"
+  ]
 }
+
